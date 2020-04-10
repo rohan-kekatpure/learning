890683cc3b8f0cd564236df06f0fde9f65608646
rohan <rohan.kekatpure@gmail.com>
2020-04-10 12:18:50 -0700

    completed pose estimation using fspy

--- cv/pose_estimation/fspy.py
+++ cv/pose_estimation/fspy.py
@@ -1,7 +1,59 @@
 import json
-import struct
-from pathlib import Path
-from IPython import embed
+from copy import deepcopy
+from struct import pack, unpack
+from subprocess import check_call
+import warnings
+
+FSPY_CMD = '/Applications/fSpy.app/Contents/MacOS/fSpy'
+
+def extract_project(file_obj, return_image_data=False):
+    """
+    Extracts state from a binary object representing an FSPY project.
+    The state is returned as a state vector.
+
+    The extraction is based on the FSPY file format which can be found at
+    https://github.com/stuffmatic/fSpy/blob/develop/project_file_format.md
+
+    The argument to this function is a file_object or a file_id, which
+    is obtained after the open() call, prior to read. So it is
+    recommended to invoke this function within a `with open(fname, 'rb') as f:`
+    block. Note that the file obj must be opened in binary mode.
+
+    Set `return_image_data` to `True` if embedded image data is needed
+    (potentially multiple MBs)
+    """
+    project_id = unpack('<I', file_obj.read(4))[0]
+    if project_id != 2037412710:
+        warnings.warn('fSpy project_id != 2037412710, likely a result of bad parsing')
+
+    project_version = unpack('<I', file_obj.read(4))[0]
+    if project_version != 1:
+        warnings.warn('fSpy project_version != 1, likely a result of bad parsing')
+
+    state_string_size = unpack('<I', file_obj.read(4))[0]
+    image_buffer_size = unpack('<I', file_obj.read(4))[0]
+
+    if image_buffer_size == 0:
+        warnings.warn('fSpy image_buffer_size == 0, fSpy project with no image data')
+
+    file_obj.seek(16)
+    state = json.loads(file_obj.read(state_string_size).decode('utf-8'))
+
+    project_info = {
+        'project': project_id,
+        'project_version': project_version,
+        'state_string_size': state_string_size,
+        'image_buffer_size': image_buffer_size,
+        'state': state,
+        'image_data': None
+    }
+
+    if return_image_data:
+        image_data = file_obj.read(image_buffer_size)
+        project_info['image_data'] = image_data
+
+    return project_info
+
 
 def _get_file_id():
     s = 'fspy'
@@ -13,34 +65,103 @@ def _get_file_id():
     return file_id
 
 
-with open('saved_state_cp.json', 'rb') as f:
-    s = json.load(f)
-    s = json.dumps(s, indent=0)
+def extract_project_from_templates():
+    with open('./project_template_1vp.fspy', 'rb') as f, \
+         open('./peoject_state_1vp.json', 'w') as g:
+        state_1vp = extract_project(f)
+        json.dump(state_1vp, g, indent=2)
+
+    with open('./project_template_2vp.fspy', 'rb') as f, \
+         open('./project_state_2vp.json', 'w') as g:
+        state_2vp = extract_project(f)
+        json.dump(state_2vp, g, indent=2)
+
+def _create_fspy_binary(state_dct, img_data, outfile_name):
+    s = json.dumps(state_dct, indent=0)
     s = s.replace('\n', '')
     state_str = s.encode('utf-8')
 
+    file_id = ('I', _get_file_id())
+    version = ('I', 1)
+    state_size = ('I', len(state_str))
+    img_buf_size = ('I', len(img_data))  # len() of a byte string is its size.
+    state_info = ('{}s'.format(state_size[1]), state_str)
+    img_info = ('{}s'.format(img_buf_size[1]), img_data)
 
-img_pth = Path('img/room4.jpg')
-with img_pth.open('rb') as f:
-    img_buf = f.read()
+    items = [file_id, version, state_size, img_buf_size, state_info, img_info]
 
-file_id = ('I', _get_file_id())
-version = ('I', 1)
-state_size = ('I', len(state_str))
-img_buf_size = ('I', img_pth.stat().st_size)
-state_info = ('{}s'.format(state_size[1]), state_str)
-img_info = ('{}s'.format(img_buf_size[1]), img_buf)
+    fmt_str = '<'
+    values = []
+    for fmt, val in items:
+        fmt_str += fmt
+        values.append(val)
 
-items = [file_id, version, state_size, img_buf_size, state_info, img_info]
+    byte_str = pack(fmt_str, *values)
 
-fmt_str = '<'
-values = []
-for fmt, val in items:
-    fmt_str += fmt
-    values.append(val)
+    with open(outfile_name, 'wb') as f:
+        f.write(byte_str)
 
-byte_str = struct.pack(fmt_str, *values)
 
-with open('scene.fspy', 'wb') as f:
-    f.write(byte_str)
+def _create_state_1vp(vp):
+    with open('state_templete_1vp.json') as f:
+        tmpl = json.load(f)
 
+    state = deepcopy(tmpl)
+    line = vp[0]
+    vp1 = state['controlPointsStateBase']['firstVanishingPoint']
+    vp1['lineSegments'] = line
+    return state
+
+
+def _create_state_2vp(vp):
+    with open('state_templete_2vp.json') as f:
+        tmpl = json.load(f)
+
+    state = deepcopy(tmpl)
+    line_1, line_2 = vp
+    vp1 = state['controlPointsStateBase']['firstVanishingPoint']
+    vp2 = state['controlPointsState2VP']['secondVanishingPoint']
+    vp1['lineSegments'] = line_1
+    vp2['lineSegments'] = line_2
+    return state
+
+def solve(img_path, scene, vanishing_segments, mode, create_fspy_binary=True):
+    vp = vanishing_segments
+    if mode == '1vp':
+        state = _create_state_1vp(vp)
+    elif mode == '2vp':
+        state = _create_state_2vp(vp)
+    else:
+        raise ValueError('Invalid mode `{}`'.format(mode))
+
+    # Compute camera parameters by invoking fspy commandline
+    state_filename = '_pre_state.json'
+    with open(state_filename, 'w') as f:
+        json.dump(state, f, indent=2)
+
+    camera_param_filename = '_camera_params.json'
+    img_width = scene['img_size']['width']
+    img_height = scene['img_size']['height']
+    cmd_list = [
+        FSPY_CMD,
+        '-w', str(img_width),
+        '-h', str(img_height),
+        '-s', state_filename,
+        '-o', camera_param_filename
+    ]
+    print(' '.join(cmd_list))
+    check_call(cmd_list)
+    with open(camera_param_filename) as f:
+        solver_result = json.load(f)
+
+    cparams = solver_result['cameraParameters']
+    state['cameraParameters'] = cparams
+    with open('_post_state.json', 'w') as f:
+        json.dump(state, f, indent=2)
+
+    if create_fspy_binary:
+        with open(img_path, 'rb') as f:
+            img_data = f.read()
+        _create_fspy_binary(state, img_data, '_project.fspy')
+
+    return cparams
--- cv/pose_estimation/vp_from_segmented_scene.py
+++ cv/pose_estimation/vp_from_segmented_scene.py
@@ -1,8 +1,15 @@
 import sys
+from collections import namedtuple
 import cv2
 import numpy as np
-import matplotlib.pyplot as pl
+np.set_printoptions(threshold=np.inf, floatmode='fixed', precision=2)
+from scipy.interpolate import splprep, splev
 from IPython import embed
+from sklearn.linear_model import LinearRegression
+from fspy import solve
+
+# Ordering of wall types is important for the logic in this code
+WALL_TYPES = ['LEFT', 'RIGHT', 'CEIL', 'FLOOR', 'FRONT']
 
 COL_FLOOR = [119, 81, 81]
 COL_CEIL = [210, 247, 241]
@@ -21,43 +28,38 @@ COL_FLOOR_HIGH = (130, 90, 90)
 COL_FRONT_LOW = (80, 60, 240)
 COL_FRONT_HIGH = (100, 80, 255)
 
-def mask_part(img, part, fill_holes=True, keep_only_largest_component=True):
-    im = img.copy()
-    im[np.abs(img - part) > 5] = 0
-    im[im > 0] = 255
-    gray = im[:, :, 0]
-    kernel = np.ones((3, 3), np.uint8)
-    gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel, iterations=3)
-    if fill_holes or keep_only_largest_component:
-        try:
-            im2, contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        except:
-            contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        if fill_holes:
-            for h, cnt in enumerate(contours):
-                cv2.drawContours(gray, [cnt], 0, 255, -1)
-            try:
-                im2, contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            except:
-                contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        if keep_only_largest_component:
-            max_area = 0.0
-            for h, cnt in enumerate(contours):
-                area = cv2.contourArea(cnt)
-                if area > max_area:
-                    max_area = area
-            for h, cnt in enumerate(contours):
-                area = cv2.contourArea(cnt)
-                if area < max_area:
-                    cv2.drawContours(gray, [cnt], 0, 0, -1)
-    return gray
+COLOR_LIMITS = {
+    'LEFT': (COL_LEFT_LOW, COL_LEFT_HIGH),
+    'RIGHT': (COL_RIGHT_LOW, COL_RIGHT_HIGH),
+    'CEIL': (COL_CEIL_LOW, COL_CEIL_HIGH),
+    'FLOOR': (COL_FLOOR_LOW, COL_FLOOR_HIGH),
+    'FRONT': (COL_FRONT_LOW, COL_FRONT_HIGH),
+}
 
-def test_mask(img):
-    gray = mask_part(img, COL_CEIL)
-    _, ax = pl.subplots(2, 1, figsize=(6, 12))
-    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    ax[1].imshow(gray, cmap='gray')
-    pl.show()
+Point = namedtuple('Point', ['x', 'y'])
+
+class Line(dict):
+    """
+    Utility class for a 2D straight line
+    """
+    def __init__(self, slope, intercept):
+        super().__init__(slope=slope, intercept=intercept)
+        self.slope = slope
+        self.intercept = intercept
+
+    def __iter__(self):
+        return iter([self.slope, self.intercept])
+
+    def __str__(self):
+        return 'Line({:0.4f}, {:0.4f})'.format(self.slope, self.intercept)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def eval(self, x):
+        m, b = self
+        return m * x + b
+
 
 def _filter_by_color_limits(image,  color_low, color_high):
     """
@@ -107,12 +109,11 @@ def _compute_convex_hull(binary_image):
     contours, _ = cv2.findContours(
         binary_image,
         mode=cv2.RETR_EXTERNAL,
-        method=cv2.CHAIN_APPROX_SIMPLE
+        method=cv2.CHAIN_APPROX_NONE
     )
 
     if len(contours) == 0:
         return np.array([])
-        # raise ValueError('No contours could be extracted from binary image')
 
     points = contours[0].reshape(-1, 2)
     hull = cv2.convexHull(points).reshape(-1, 2)
@@ -122,47 +123,236 @@ def _compute_convex_hull(binary_image):
 
     return hull
 
+def _get_line_fit(x, y):
+    lr = LinearRegression(fit_intercept=True, n_jobs=1)
+    lr.fit(x.reshape(-1, 1), y)
+    m, b = lr.coef_[0], lr.intercept_
+    return Line(m, b)
 
-def main():
-    if len(sys.argv) < 2:
-        print('Usage: python {} <image_path>'.format(sys.argv[0]))
-        exit(1)
+def _get_intersection(line1, line2):
+    m1, b1 = line1
+    m2, b2 = line2
+    # TODO: catch the condition if m1 == m2
+    x_intersect = -(b2 - b1) / (m2 - m1)
+    y_intersect = (m2 * b1 - m1 * b2) / (m2 - m1)
+    return Point(x_intersect, y_intersect)
 
-    img = cv2.imread(sys.argv[1])
-    f = 600.0 / img.shape[1]
-    img = cv2.resize(img, None, fx=f, fy=f)
+def _compute_vp_from_poly4(hull, wall):
+    """
+    Computes vanishing point (VP) from the convex hull `hull`.
+    The convex hull of wall is assumed to be a 4-gon (quadrilateral)
+    """
+    x0, y0 = hull[:, 0], hull[:, 1]
+    n_points = 1000
+    tck, _ = splprep([x0, y0], s=0, per=1, k=1)
+    u1 = np.linspace(0, 1, n_points)
+    x1, y1 = splev(u1, tck)
+    dx = np.diff(x1, append=x1[-1])
+    dy = np.diff(y1, append=y1[-1])
+    angles = np.arctan2(dy, dx)
+
+    # We now divide up the 4 quadrants into 5 regions as follows:
+    # R1 : -pi to -pi/2 - eps
+    # R2: -pi/2 - eps to -pi/2 + eps
+    # R3: -pi/2 + eps to pi/2 - eps
+    # R4: pi/2 - eps to pi/2 + eps
+    # R5: pi/2 + eps to pi
+    #
+    # In other words, small angular regions around theta = -pi/2
+    # and theta = +pi/2 are R2 and R4 respectively. We ignore
+    # any points in R2 and R4 because they are clipped by image
+    # boundaries. R1 and R5 are really a single region to the
+    # left of the y-axis. We only include points from R3 and
+    # R1 union R5 (R1u5). We fit a line to points belonging to R3
+    # and fit a separate line to points in R1u5. The intersection of
+    # these lines is the vanishing point.
+
+    eps = 0.05  # Heuristic; angle tolerence
+    p = np.pi
+    # regions = [-p, -p/2 - eps, -p/2 + eps, p/2 - eps, p/2 + eps, p]
+    # assignments = np.digitize(angles, regions)
+    # Ra = assignments == 3
+    # Rb = (assignments == 1) | (assignments == 5)
+
+    regions = [-p, -p + eps, -p/2 - eps, -p/2 + eps, -eps, eps, p/2 - eps, p/2 + eps, p - eps, p]
+    assignments = np.digitize(angles, regions)
+    if wall in ['LEFT', 'RIGHT', 'FRONT']:
+        Ra = (assignments == 4) | (assignments == 6)
+        Rb = (assignments == 2) | (assignments == 8)
+    elif wall in ['CEIL', 'FLOOR']:
+        Ra = (assignments == 6) | (assignments == 8)
+        Rb = (assignments == 2) | (assignments == 4)
+    else:
+        raise ValueError('Invalid wall: {}'.format(wall))
+
+    if (x1[Ra].shape[0] == 0) or (x1[Rb].shape[0] == 0):
+        embed()
+
+    line_a = _get_line_fit(x1[Ra], y1[Ra])
+    line_b = _get_line_fit(x1[Rb], y1[Rb])
+    p_int = _get_intersection(line_a, line_b)
+    return line_a, line_b, p_int
+
+def _visualize(img, scene):
+    """
+    Only used for debugging purposes, disable in prod
+    """
+    import matplotlib.pyplot as pl
     H, W = img.shape[:2]
+    pl.imshow(img)
+    colors = ['r', 'g', 'b', 'm', 'k']
+    VPs = []
+    for i, wall_type in enumerate(WALL_TYPES):
+        if wall_type in ['CEIL', 'FLOOR', 'FRONT']:
+            continue
 
-    _, ax = pl.subplots(2, 3)
-    ax = ax.ravel()
+        wall_data = scene[wall_type]
+        color = colors[i]
 
-    walls = ['LEFT', 'RIGHT', 'CEIL', 'FLOOR', 'FRONT']
-    limits = {
-        'LEFT': (COL_LEFT_LOW, COL_LEFT_HIGH),
-        'RIGHT': (COL_RIGHT_LOW, COL_RIGHT_HIGH),
-        'CEIL': (COL_CEIL_LOW, COL_CEIL_HIGH),
-        'FLOOR': (COL_FLOOR_LOW, COL_FLOOR_HIGH),
-        'FRONT': (COL_FRONT_LOW, COL_FRONT_HIGH),
+        if not wall_data['present']:
+            continue
+
+        # Compute the lines of the wall
+        x = np.linspace(0, W, 200)
+        y1 = wall_data['line1'].eval(x)
+        y2 = wall_data['line2'].eval(x)
+        pl.plot(x, y1, '-', color=color, lw=2)
+        pl.plot(x, y2, '-', color=color, lw=2)
+
+        # Plot vanishing point
+        vp = wall_data['vp']
+        VPs.append(vp)
+        pl.plot(vp[0], vp[1], '*', color=color, ms=10)
+
+    if len(VPs) == 2:
+        pl.plot([VPs[0].x, VPs[1].x], [VPs[0].y, VPs[1].y], 'k--', lw=2)
+
+    pl.xlim([0, W])
+    pl.ylim([0, H])
+    pl.axis('off')
+    pl.show()
+
+def _compute_scene(segmented_img):
+    """
+    Computes scene info from the segmented image. Scene info
+    is a large dictionary containing information about every
+    wall found in the segmented image. The computed wall information
+    includes presence, area fraction, vanishing lines, and
+    vanishing point. An example scene info
+    dictionary is below:
+
+    {
+      "img_size": (1000, 1024),
+      "LEFT": {
+        "present": true,
+        "frac": 0.10804444444444444,
+        "line1": {
+          "slope": 0.6085912085025705,
+          "intercept": 8.191525463296863
+        },
+        "line2": {
+          "slope": -0.9146919935648665,
+          "intercept": 361.84396656582277
+        },
+        "vp": [
+          232.1646038126989,
+          149.48486226918777
+        ]
+      },
+      "RIGHT": {
+        "present": true,
+        "frac": 0.047822222222222224,
+        "line1": {
+          "slope": -0.3399272854756361,
+          "intercept": 262.6780737743618
+        },
+        "line2": {
+          "slope": 0.987671401400012,
+          "intercept": -268.3573785682675
+        },
+        "vp": [
+          399.9969701629945,
+          126.7081895083761
+        ]
+      },
+      "CEIL": {
+        "present": true,
+        "frac": 0.15413703703703704,
+        "line1": {
+          "slope": -0.44868160099867666,
+          "intercept": 322.3868838317588
+        },
+        "line2": {
+          "slope": 0.6064486465795312,
+          "intercept": 9.743060715650913
+        },
+        "vp": [
+          296.308274579091,
+          189.43881280445675
+        ]
+      },
+      "FLOOR": {
+        "present": true,
+        "frac": 0.3711925925925926,
+        "line1": {
+          "slope": 0.8990067401542693,
+          "intercept": -220.11268477979291
+        },
+        "line2": {
+          "slope": -0.8022267320474438,
+          "intercept": 356.0939096030923
+        },
+        "vp": [
+          338.6993048268481,
+          84.38027314510894
+        ]
+      },
+      "FRONT": {
+        "present": true,
+        "frac": 0.30666666666666664,
+        "line1": {
+          "slope": 0.009753618110430596,
+          "intercept": 79.48569795024093
+        },
+        "line2": {
+          "slope": -0.13748149945364022,
+          "intercept": 311.54045703346105
+        },
+        "vp": [
+          1576.0829544095634,
+          94.85820919791101
+        ]
+      }
     }
 
-    ax[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    """
+    img = segmented_img.copy()
+    H, W = img.shape[:2]
+    scene = dict.fromkeys(['img_size'] + WALL_TYPES)
+    scene['img_size'] = {'height': H, 'width': W}
+    for i, wall_type in enumerate(WALL_TYPES):
+        wall_data = dict.fromkeys(['present', 'frac', 'line1', 'line2', 'vp'])
+        scene[wall_type] = wall_data
 
-    for i, wall in enumerate(walls):
-        low = limits[wall][0]
-        high = limits[wall][1]
+        # Initially a wall is assumed to be absent, will be changed
+        # to true if detected
+        wall_data['present'] = False
+
+        low = COLOR_LIMITS[wall_type][0]
+        high = COLOR_LIMITS[wall_type][1]
         mask = _filter_by_color_limits(img.copy(), low, high)
         n_comps, comp_map = cv2.connectedComponents(mask, connectivity=4)
 
         if n_comps == 1:  # Wall not found
-            max_area = W * H
-            max_label = 0
+            continue
         else:
             max_area, max_label = _get_largest_connected_component(comp_map)
+            wall_data['frac'] = max_area / (W * H)
 
         # If the largest component has a very small area it is invalid
         # In this case, found no  component at all, the component map
         # is zeroed out
-        if not _is_component_valid(max_area, W, H, frac=0.05):
+        if not _is_component_valid(max_area, W, H, frac=0.02):
             comp_map *= 0
 
         # In case there are multiple components detected, zero out
@@ -176,23 +366,89 @@ def main():
         # Take convex hull of the comp_map
         hull = _compute_convex_hull(comp_map)
 
-        # Filter new image with the comp_map as the mask
-        new_img = cv2.bitwise_and(img, img, mask=comp_map)
-
-        print('{}, frac = {}'.format(wall, max_area / (W * H)))
-        curr_ax = ax[i + 1]
-        curr_ax.imshow(cv2.cvtColor(new_img, cv2.COLOR_BGR2RGB))
-
         if len(hull) > 0:
-            curr_ax.plot(hull[:, 0], hull[:, 1], '-', color='#41fdfe', lw=2)
+            wall_data['present'] = True
+            line1, line2, vp = _compute_vp_from_poly4(hull, wall_type)
+            wall_data['line1'] = line1
+            wall_data['line2'] = line2
+            wall_data['vp'] = vp
 
-        curr_ax.text(0.5, 0.5, wall, fontsize=15, color='green', transform=curr_ax.transAxes)
+    return scene
 
-        # exit(0)
+def _assign_vp_mode(scene):
+    room_type = set()
+    for wt in WALL_TYPES:
+        if scene[wt]['present']:
+            room_type.add(wt)
 
-    for a in ax:
-        a.axis('off')
-    pl.show()
+    if room_type == {'LEFT', 'RIGHT', 'CEIL', 'FLOOR', 'FRONT'}:
+        vp_mode = '1vp'
+        vp_walls = ['LEFT']
+    elif room_type == {'LEFT', 'RIGHT', 'CEIL', 'FLOOR'}:
+        vp_mode = '2vp'
+        vp_walls = ['LEFT', 'RIGHT']
+    elif room_type == {'RIGHT', 'LEFT', 'CEIL', 'FRONT'}:
+        vp_mode = '1vp'
+        vp_walls = ['CEIL']
+    elif room_type == {'RIGHT', 'LEFT', 'FLOOR', 'FRONT'}:
+        vp_mode = '1vp'
+        vp_walls = ['FRONT']
+    else:
+        vp_mode = 'default'
+        vp_walls = []
+
+    return vp_mode, vp_walls
+
+def _compute_vanishing_segments(scene, vp_walls):
+    """
+    Computes vanishing segments for the assigned walls
+    in the scene. Even though the scene object already
+    contains vanishing points explicitly, fSpy requires
+    vanishing points to be represented as two segments
+    (4 points). Therefore we need need to recompute the
+    two-segment representation for the walls that are
+    assigned by `_assign_vp_mode()`
+    """
+    vp = []
+    for wall_type in vp_walls:
+        img_size = scene['img_size']
+        W = img_size['width']
+        H = img_size['height']
+        a = float(W) / float(H)
+        wall_info = scene[wall_type]
+        m1, b1 = wall_info['line1']
+        m2, b2 = wall_info['line2']
+        x11, x12 = 0.25, 0.75
+        x21, x22 = 0.25, 0.75
+        y11 = m1 * a * x11 + b1 / H
+        y12 = m1 * a * x12 + b1 / H
+        y21 = m2 * a * x21 + b2 / H
+        y22 = m2 * a * x22 + b2 / H
+
+        segment = [
+            [{'x': x11, 'y': y11}, {'x': x12, 'y': y12}],
+            [{'x': x21, 'y': y21}, {'x': x22, 'y': y22}]
+        ]
+        vp.append(segment)
+
+    return vp
+
+def compute_camera_parameters(orig_img_pth, segmented_img_pth):
+    segmented_img = cv2.imread(segmented_img_pth)
+    scene = _compute_scene(segmented_img)
+    vp_mode, vp_walls = _assign_vp_mode(scene)
+    vp = _compute_vanishing_segments(scene, vp_walls)
+    params = solve(orig_img_pth, scene, vp, mode=vp_mode)
+    print(params)
+    # _visualize(img, scene)
+
+def example():
+    if len(sys.argv) < 2:
+        print('Usage: python {} <image_path>'.format(sys.argv[0]))
+        exit(1)
+    segmented_img_path = sys.argv[1]
+    orig_img_path = sys.argv[2]
+    compute_camera_parameters(orig_img_path, segmented_img_path)
 
 if __name__ == '__main__':
-    main()
+    example()
